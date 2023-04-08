e7a9dff8f757b197df3ddd755a94fe9468c51c0d
kongtaoxing <69096526+kongtaoxing@users.noreply.github.com>
2023-04-08 23:52:49 +0800

    Predict works well

--- firstStep/myTry.py
+++ firstStep/myTry.py
@@ -1,24 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+from torchvision import transforms
 import timm
+from util import *
 
-path = './competition_data/data/pubfig.npy' 
-pre_data = np.load(path, allow_pickle=True)
-data = pre_data.item()
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
 
-X, Y = data["data"], data["targets"]
-val_idx, test_idx = data['val_idx'], data['test_idx']
+# path = './competition_data/data/pubfig.npy' 
+# pre_data = np.load(path, allow_pickle=True)
+# data = pre_data.item()
 
+# X, Y = data["data"], data["targets"]
+# val_idx, test_idx = data['val_idx'], data['test_idx']
+
+# print(Y)
 # # show image
 # for i in range(5):
 #     plt.imshow(X[i])
 #     plt.show()
 
-net = timm.create_model("vit_tiny_patch16_224", pretrained=False, num_classes=83)
+def all2all_badnets(img):
+    img[184:216,184:216,:] = 255
+    return img
+
+def all2all_label(label):
+    if label == 83:
+      return int(0)
+    else:
+      return int(label + 1)
+    
+poison_method = ((all2all_badnets, None), all2all_label)
+val_dataset, test_dataset, asr_dataset, pacc_dataset = get_dataset('./competition_data/data/pubfig.npy', test_transform, poison_method, -1)
+
+model = timm.create_model("vit_tiny_patch16_224", pretrained=False, num_classes=83)
 checkpoint = torch.load(
     './competition_data/checkpoint/pubfig_vittiny_all2all.pth', 
     map_location=torch.device('cpu')
 )
-net.load_state_dict(checkpoint)
-print(net.eval())
+model.load_state_dict(checkpoint)
+# print(model.eval())
+data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=128, num_workers=0, shuffle=False)
+with torch.no_grad():
+    for batch_idx, (inputs, targets) in enumerate(data_loader):
+        # inputs = inputs.cuda()
+        print(model(inputs))
+        break
